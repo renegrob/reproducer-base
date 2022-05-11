b2b25ffc66c2b1aa01c3c135f20f79a137fd5303
René Grob <rene.grob@ti8m.ch>
2022-05-11 10:19:28 +0200

    port bind excception

--- src/test/java/com/github/renegrob/GreetingResourceTest.java
+++ src/test/java/com/github/renegrob/GreetingResourceTest.java
@@ -1,16 +1,36 @@
 package com.github.renegrob;
 
-import io.quarkus.test.junit.QuarkusTest;
+import java.net.ServerSocket;
+import java.net.URL;
+import javax.inject.Inject;
+
+import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+@ExtendWith(value = GreetingResourceTest.PortBlockerExtension.class)
 @QuarkusTest
 public class GreetingResourceTest {
 
+    @Inject
+    Logger log;
+
+    @TestHTTPResource
+    URL url;
+
     @Test
     public void testHelloEndpoint() {
+        log.info(url);
         given()
           .when().get("/hello-resteasy")
           .then()
@@ -18,4 +38,22 @@ public class GreetingResourceTest {
              .body(is("Hello RESTEasy"));
     }
 
+    static class PortBlockerExtension implements Extension, BeforeAllCallback, AfterAllCallback {
+
+        private ServerSocket serverSocket;
+
+        @Override
+        public void afterAll(ExtensionContext context) throws Exception {
+            if (serverSocket != null) {
+                serverSocket.close();
+                serverSocket = null;
+            }
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext context) throws Exception {
+            System.setProperty("quarkus.http.test-port", "0");
+            serverSocket = new ServerSocket(8081);
+        }
+    }
 }
